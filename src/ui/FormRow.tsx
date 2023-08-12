2b87d21d3aaee873a977b2b6762b39bfa5a6086e
hoangtd21 <tranduchoang21399@gmail.com>
2023-08-12 15:18:52 +0700

    Handle error form

--- src/ui/FormRow.tsx
+++ src/ui/FormRow.tsx
@@ -1,12 +1,10 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 const StyledFormRow = styled.div`
   display: grid;
   align-items: center;
-
-  grid-template-columns: ${(props) =>
-    props.orientation === 'vertical' ? '1fr' : '24rem 1fr 1.2fr'};
-  gap: ${(props) => (props.orientation === 'vertical' ? '0.8rem' : '2.4rem')};
+  grid-template-columns: 24rem 1fr 1.2fr;
+  gap: 2.4rem;
 
   padding: 1.2rem 0;
 
@@ -19,22 +17,14 @@ const StyledFormRow = styled.div`
   }
 
   &:not(:last-child) {
-    border-bottom: ${(props) =>
-      props.orientation === 'vertical'
-        ? 'none'
-        : '1px solid var(--color-grey-100)'};
+    border-bottom: 1px solid var(--color-grey-100);
   }
 
-  /* Special treatment if the row contains buttons, and if it's NOT a vertical row */
-  ${(props) =>
-    props.orientation !== 'vertical' &&
-    css`
-      &:has(button) {
-        display: flex;
-        justify-content: flex-end;
-        gap: 1.2rem;
-      }
-    `}
+  &:has(button) {
+    display: flex;
+    justify-content: flex-end;
+    gap: 1.2rem;
+  }
 `;
 
 const Label = styled.label`
@@ -46,9 +36,17 @@ const Error = styled.span`
   color: var(--color-red-700);
 `;
 
-function FormRow({ label, error, children, orientation }) {
+interface FormRowProps {
+  label?: string;
+  error?: string;
+  // children: React.ReactNode;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  children: any;
+}
+
+function FormRow({ label, error, children }: FormRowProps) {
   return (
-    <StyledFormRow orientation={orientation}>
+    <StyledFormRow>
       {label && <Label htmlFor={children.props.id}>{label}</Label>}
       {children}
       {error && <Error>{error}</Error>}
