1267c33f735d3d51cd7cba9cfcf84c8efa0d09d4
hoangtd21 <tranduchoang21399@gmail.com>
2023-09-17 09:47:49 +0700

    Filter and sort bookings in client

--- src/features/bookings/BookingTable.tsx
+++ src/features/bookings/BookingTable.tsx
@@ -1,12 +1,46 @@
-import BookingRow from './BookingRow';
+import BookingRow, { BookingI } from './BookingRow';
 import Table from '../../ui/Table';
 import Menus from '../../ui/Menus';
 import Empty from '../../ui/Empty';
 import useBookings from './useBookings';
 import Spinner from '../../ui/Spinner';
+import { useSearchParams } from 'react-router-dom';
 
 function BookingTable() {
   const { bookings, isLoading } = useBookings();
+  const [searchParams] = useSearchParams();
+  let filteredBookings: BookingI[] | undefined;
+
+  // Filter
+  const filterValue = searchParams.get('status') || 'all';
+  if (filterValue === 'all') filteredBookings = bookings;
+  if (filterValue === 'checked-out')
+    filteredBookings = bookings?.filter(
+      (booking) => booking.status === 'checked-out'
+    );
+  if (filterValue === 'checked-in')
+    filteredBookings = bookings?.filter(
+      (booking) => booking.status === 'checked-in'
+    );
+  if (filterValue === 'unconfirmed')
+    filteredBookings = bookings?.filter(
+      (booking) => booking.status === 'unconfirmed'
+    );
+
+  // Sort
+  const sortBy = searchParams.get('sortBy') || 'Sort by date (recent first)';
+  const [field, direction] = sortBy.split('-') as [keyof BookingI, string];
+  const modifier = direction === 'asc' ? 1 : -1;
+  const sortedBookings = filteredBookings?.sort((a, b) => {
+    const valueA =
+      typeof a[field] === 'string' ? String(a[field]).toLowerCase() : a[field];
+    const valueB =
+      typeof b[field] === 'string' ? String(b[field]).toLowerCase() : b[field];
+    if (valueA < valueB) return -1 * modifier;
+    if (valueA > valueB) return 1 * modifier;
+    return 0;
+  });
+
   if (!bookings?.length) return <Empty resourceName="bookings" />;
   if (isLoading) return <Spinner />;
 
@@ -23,7 +57,7 @@ function BookingTable() {
         </Table.Header>
 
         <Table.Body
-          data={bookings}
+          data={sortedBookings ? sortedBookings : []}
           render={(booking) => (
             <BookingRow key={booking.id} booking={booking} />
           )}
