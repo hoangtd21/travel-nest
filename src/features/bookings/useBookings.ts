54efdc8142c92a2e18a8628543fd1cd4e676dc40
hoangtd21 <tranduchoang21399@gmail.com>
2023-09-18 23:11:32 +0700

    Prefetch bookings

--- src/features/bookings/useBookings.ts
+++ src/features/bookings/useBookings.ts
@@ -1,10 +1,12 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { getBookings } from '../../services/apiBookings';
 import { useSearchParams } from 'react-router-dom';
 import { BookingI } from './BookingRow';
+import { PAGE_SIZE } from '../../ui/Pagination';
 
 export default function useBookings() {
   const [searchParams] = useSearchParams();
+  const queryClient = useQueryClient();
 
   // Filter
   const filterValue = searchParams.get('status');
@@ -29,6 +31,22 @@ export default function useBookings() {
     queryKey: ['bookings', filter, sortBy, page],
     queryFn: () => getBookings({ filter, sortBy, page }),
   });
+
+  const pageCount = Math.ceil(count / PAGE_SIZE);
+
+  if (page < pageCount) {
+    queryClient.prefetchQuery({
+      queryKey: ['bookings', filter, sortBy, page + 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+    });
+  }
+
+  if (page > 1) {
+    queryClient.prefetchQuery({
+      queryKey: ['bookings', filter, sortBy, page - 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
+    });
+  }
 
   return { bookings, isLoading, error, count };
 }
