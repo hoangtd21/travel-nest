f12b72f9b6cddb78d5bab1b0cbf2c6c784b95ced
hoangtd21 <tranduchoang21399@gmail.com>
2023-10-01 22:08:11 +0700

    Add checkin page

--- src/features/check-in-out/CheckinBooking.tsx
+++ src/features/check-in-out/CheckinBooking.tsx
@@ -1,38 +1,42 @@
-import styled from 'styled-components';
+// import styled from 'styled-components';
 import BookingDataBox from '../bookings/BookingDataBox';
 
-import Row from '../../ui/Row';
-import Heading from '../../ui/Heading';
-import ButtonGroup from '../../ui/ButtonGroup';
 import Button from '../../ui/Button';
+import ButtonGroup from '../../ui/ButtonGroup';
 import ButtonText from '../../ui/ButtonText';
+import Heading from '../../ui/Heading';
+import Row from '../../ui/Row';
 
 import { useMoveBack } from '../../hooks/useMoveBack';
+import Spinner from '../../ui/Spinner';
+import useBooking from '../bookings/useBooking';
+import { useParams } from 'react-router-dom';
 
-const Box = styled.div`
-  /* Box */
-  background-color: var(--color-grey-0);
-  border: 1px solid var(--color-grey-100);
-  border-radius: var(--border-radius-md);
-  padding: 2.4rem 4rem;
-`;
+// const Box = styled.div`
+//   /* Box */
+//   background-color: var(--color-grey-0);
+//   border: 1px solid var(--color-grey-100);
+//   border-radius: var(--border-radius-md);
+//   padding: 2.4rem 4rem;
+// `;
 
 function CheckinBooking() {
+  const { booking, isLoading } = useBooking();
+  const { bookingId } = useParams();
   const moveBack = useMoveBack();
 
-  const booking = {};
-
-  const {
-    id: bookingId,
-    guests,
-    totalPrice,
-    numGuests,
-    hasBreakfast,
-    numNights,
-  } = booking;
+  // const {
+  //   id,
+  //   // guests,
+  //   // totalPrice,
+  //   // numGuests,
+  //   // hasBreakfast,
+  //   // numNights,
+  // } = booking as BookingI;
 
   function handleCheckin() {}
 
+  if (isLoading) return <Spinner />;
   return (
     <>
       <Row type="horizontal">
@@ -40,11 +44,13 @@ function CheckinBooking() {
         <ButtonText onClick={moveBack}>&larr; Back</ButtonText>
       </Row>
 
-      <BookingDataBox booking={booking} />
+      {booking && <BookingDataBox booking={booking} />}
 
       <ButtonGroup>
-        <Button onClick={handleCheckin}>Check in booking #{bookingId}</Button>
-        <Button variation="secondary" onClick={moveBack}>
+        <Button variation="primary" size="medium" onClick={handleCheckin}>
+          Check in booking #{bookingId}
+        </Button>
+        <Button variation="secondary" size="medium" onClick={moveBack}>
           Back
         </Button>
       </ButtonGroup>
